55986ecbf6c10b495fbbdebdafb8682949e12491
Pawit Manussirivitaya <democryst@gmail.com>
2017-08-10 13:54:32 +0700

    homework

---
+++ app/src/main/java/com/example/tngp17_001/work01_testuiandlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tngp17_001.work01_testuiandlayout;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
